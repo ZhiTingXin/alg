61603f9971a4d74270ef6c0180dd9bec2d09ebf3
hacker <151250170@smail.nju.edu.cn>
2024-08-28 23:57:29 +0800

    question84

--- src/main/java/com/test/algorithm/Question84.java
+++ src/main/java/com/test/algorithm/Question84.java
@@ -1,5 +1,8 @@
 package com.test.algorithm;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * 84. 柱状图中最大的矩形
  * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
@@ -25,10 +28,28 @@ public class Question84 {
     }
     public int largestRectangleArea2(int[] heights) {
         // 单调栈实现
-
+        int[] leftIndex = new int[heights.length];
+        int[] rightIndex = new int[heights.length];
+        Arrays.fill(rightIndex, heights.length);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                // 比我大不需要care
+                rightIndex[stack.peek()] = i;
+                stack.pop();
+            }
+            leftIndex[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            maxArea = Math.max(maxArea, (rightIndex[i] - leftIndex[i] - 1) * heights[i]);
+        }
+        return maxArea;
     }
     public static void main(String[] args) {
         Question84 q = new Question84();
         System.out.println(q.largestRectangleArea(new int[]{2,1,5,6,2,3}));
+        System.out.println(q.largestRectangleArea2(new int[]{2,1,5,6,2,3}));
     }
 }
